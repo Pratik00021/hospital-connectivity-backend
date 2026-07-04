24226e46edaf1ab14e2fe901c6409e4a60a8b989
Pratik Badwaik <pratikbadwaik737@gmail.com>
2026-07-04 16:59:34 +0530

    Enable SSL for hosted PostgreSQL connections

--- config/db.js
+++ config/db.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isLocal = process.env.PGHOST === "localhost" || process.env.PGHOST === "127.0.0.1";
+
 const pool = new pg.Pool({
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
+  ssl: isLocal ? false : { rejectUnauthorized: false },
 });
 
 pool.on("error", (err) => {
